6d4d506186411f8a4c24508037fe6df6e0eef8d8
DMJennings <dakota96j@gmail.com>
2023-04-21 14:38:22 -0600

    added pwd command and nodemon for npm

--- bash.js
+++ bash.js
@@ -1,10 +1,20 @@
 //Output a prompt
+
 process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', (data) => {
+    
   const cmd = data.toString().trim(); //remove the newline
-
-  process.stdout.write('You typed: ' + cmd);
+  if(cmd == "pwd") {
+    process.stdout.write(process.cwd());
+  }
+  
+  process.stdout.write('\nYou typed: ' + cmd);
+  
   process.stdout.write('\nprompt > ');
 });
+
+
+// import { cwd } from 'node:process';
+// console.log(`Current directory: ${cwd()}`);
